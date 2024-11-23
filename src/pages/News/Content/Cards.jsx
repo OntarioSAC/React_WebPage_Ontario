84f67413b052fd01254e14ed183632d338540fcf
Farid Lucio Gonzales Gonzalo <40509906+shadow3dcoo21@users.noreply.github.com>
2024-11-23 12:16:01 -0500

    nuevos cambios

--- src/pages/News/Content/Cards.jsx
+++ src/pages/News/Content/Cards.jsx
@@ -1,5 +1,5 @@
 
-import { right } from '@popperjs/core';
+import { bottom, right } from '@popperjs/core';
 import { Button } from 'bootstrap';
 
 import { useNavigate } from "react-router-dom";
@@ -8,34 +8,45 @@ import data from "../../../data/new-content-cards.json"; // Asegúrate de que es
 
 import React from 'react';
 
+const generalcardbody = {
+   backgroundColor: 'white',
+   display:'flex',
+   flexDirection:'row',
+   justifyContent:'center',
+   height:'1600px'
+};
+const bodycard ={
+   backgroundColor:'white',
+   width: '45%',
+   position:'relative',
+   display:'flex',
+   flexDirection:'row',
+   rowGap: '72px', 
+   columnGap: '36px', 
+   flexWrap: 'wrap',
+   justifyContent: 'center'
+};
   const titleStyle = {
     fontSize: 15,
     
-    
-
   };
   
- const cuerpo ={
-    backgroundColor:'white',
-    height: '600px',
-    position:'relative',
-    display:'flex',
-    flexDirection:'row',
-    gap:10
- }
-;
-const tarjeta ={
-    backgroundColor: 'white',
+
+const cards ={
+    backgroundColor: 'orange',
     height:'501px',
     width: '374px',
-    borderRadius: '15px'
+    borderTopLeftRadius:30,
+    borderTopRightRadius:30,
+    borderBottomRightRadius:30,
+    boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)"
 
  };
  const imagen_padre={
     backgroundColor: 'transparent',
-    borderTopRightRadius: '15px',
-    borderTopLeftRadius: '15px',
-    borderBottomLeftRadius: '19px',
+    borderTopRightRadius: '30px',
+    borderTopLeftRadius: '30px',
+    borderBottomLeftRadius: '30px',
     height: '216px',
     width: '100%',
     overflow:'hidden'
@@ -45,27 +56,27 @@ const tarjeta ={
     height: '100%',
     width: '100%',
     
-    
-    
-    
-
  };
  const h1_text ={
-    backgroundColor: 'purple',
+    backgroundColor: 'transparent',
     width: '85%',
-    
-    
-    
+    fontFamily: "Inter",
+    lineHeight: "normal",
+    fontStyle: "normal",
     
 
  };
  const contenidotext ={
-    backgroundColor: 'green',
+    backgroundColor: 'white',
     width: '85%',
-    
-    
-    
-    
+    color: "#1C284C",
+   fontFamily: "Inter",
+   fontSize: "15px",
+   fontStyle: "normal",
+   fontWeight: 400,
+   lineHeight: "normal",
+
+ 
 
  };
  const h1_text_padre ={
@@ -85,42 +96,127 @@ const tarjeta ={
  const texto ={
    
     fontSize:'15px'
-    
-    
-    
-
- };
- const boton ={
    
-    backgroundColor: 'orange',
-    fontSize:'13px',
-    fontWeigh:400,
-    display:'flex',
-    padding:5,
-    justifyContent: 'flex-end',
-    
-    
-    
-
+ };
+ const boton ={ 
+    backgroundColor: 'transparent',
+    color: "#1C284C",
+    fontFamily: "Inter",
+    fontSize: "13px", 
+    fontWeight: 900,
+    lineHeight: "normal",
  };
  const contenedor_boton = {
-    backgroundColor: 'red',
-    width: '50%',
-    position:'relative',
+    backgroundColor: '#CBF000',
+    width: '135px',
+    height:'35.218px',
+    justifyContent:'center',
+    alignItems:'center',
+    display:'flex',
+    borderRadius:'37.867px',
+    flexDirection:'row',
+    position:'absolute',
     
 };
-const h1_text_titulo = {
+const contenedor_boton_general={
+   width: '90%',
+   display:'flex',
    backgroundColor: 'blue',
-   fontWeigh:800,
-   fontSize:"20px"
+   justifyContent: 'flex-end',
+   
+}
+const h1_text_titulo = {
+   backgroundColor: 'white',
+   fontWeight: 800,
+   fontSize:"20px",
+   color:'#1C284C'
 
    
 };
+
+const derecha_ = {
+   backgroundColor: 'white',
+   display:'flex',
+   flexDirection:'column',
+   gap:32 
+};
+const derecha_int = {
+   backgroundColor: 'white',
+   width: '374px',
+   height:'190px',
+   borderTopRightRadius: '30px',
+    borderTopLeftRadius: '30px',
+    borderBottomLeftRadius: '30px',
+    border: '2px solid #1C284C',
+    display:'flex',
+    justifyContent:'center',
+    alignItems:'center',
+    flexDirection:'column',
+    gap:35
+};
+const derecha_int_text = {
+   backgroundColor: 'white',
+   width: '90%',
+};
+const text_texto_dere={
+   color:'#1C284C',
+   fontFamily: "Inter",
+   fontSize: "20px",
+   fontStyle: "normal",
+   fontWeight: 800,
+   lineHeight: "normal",
+}
+const derecha_int_fecha={
+   color:'#1C284C',
+   fontFamily: "Inter",
+   fontSize: "16px",
+   fontStyle: "normal",
+   fontWeight: 600,
+   lineHeight: "normal",
+  
+}
+const derecha_int_fecha_cuerpo={
+   backgroundColor: 'white',
+   width: '90%',
+  
+}
+const titulo_derecha={
+   paddingTop:15,
+   paddingBottom: 15,
+   color:'#1C284C',
+   fontFamily: "Inter",
+   fontSize: "24px",
+   fontStyle: "normal",
+   fontWeight: 600,
+   lineHeight: "normal",
+
+}
+const titulo_derecha_in={
+   paddingTop:15,
+   paddingBottom: 15,
+   color:'#1C284C',
+   fontFamily: "Inter",
+   fontSize: "24px",
+   fontStyle: "normal",
+   fontWeight: 900,
+   lineHeight: "normal",
+
+}
+const Titulo_general={
+   display:'flex',
+   flexDirection:'row',
+   gap:7
+
+}
 const Cards = () => {
   return (
-   <div style={cuerpo}>
+   
+<div style={generalcardbody}>
+
+<div style={bodycard}>
    {data.cards.map((card) => (
-     <div key={card.id} style={tarjeta}>
+     <div key={card.id} style={cards}>
+
        {/* Imagen */}
        <div style={imagen_padre}>
          <img
@@ -129,28 +225,66 @@ const Cards = () => {
            style={imagen_}
          />
        </div>
+
        {/* Título */}
        <div style={h1_text_padre}>
+
          <div style={h1_text}>
            <p style={h1_text_titulo}>{card.title}</p>
          </div>
+
          {/* Descripción */}
          <div style={contenidotext}>
            <p style={texto}>{card.description}</p>
          </div>
+
          {/* Botón */}
-         <div style={contenedor_boton}>
-           <p
-             style={boton}
-             onClick={() => handleRedirect(card.redirectTo)}
-           >
-             {card.buttonText || "Leer Más"}
-           </p>
+         <div style={contenedor_boton_general}>
+            <div style={contenedor_boton}>
+
+               <p style={boton}>
+                  {card.buttonText}
+               </p>
+
+               <p>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="9" height="15" viewBox="0 0 9 15" fill="none">
+                     <path d="M0.774162 12.6663L5.59297 7.84749L0.774161 3.02868C0.289797 2.54432 0.289797 1.76188 0.774161 1.27752C1.25853 0.793155 2.04096 0.793155 2.52533 1.27752L8.22592 6.97812C8.71029 7.46248 8.71029 8.24492 8.22592 8.72928L2.52533 14.4299C2.04096 14.9142 1.25853 14.9142 0.774162 14.4299C0.302217 13.9455 0.289797 13.1507 0.774162 12.6663Z" fill="#1C284C"/>
+                  </svg>
+               </p>
+               
+            </div>
          </div>
+         
        </div>
      </div>
+   
+   ))}
+</div>
+
+
+
+ <div style={derecha_}>
+   <div style={Titulo_general}>
+      <h5 style={titulo_derecha}>Publicaciones </h5>
+      <p style={titulo_derecha_in}>Recientes</p>
+   </div>
+   {data.notiMini.map((noti) => (
+   <div key={noti.id}  style={derecha_int}>
+      <div style={derecha_int_text}>
+         <h2 style={text_texto_dere}>{noti.title}</h2>
+      </div>
+      <div style={derecha_int_fecha_cuerpo} >
+         <p style={derecha_int_fecha}>{noti.fecha}</p>
+      </div>
+      
+   </div>
    ))}
  </div>
+
+ </div>
+
+
+
   );
 };
 
--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -4,14 +4,13 @@ import NewsBanner from './Content/NewsBanner';
 import NewsTabs from './Content/NewsTabs';
 import Cards from './Content/Cards';
 
+
 const News = () => {
   return (
     <div>
       <NewsBanner />
-        
       <NewsTabs />
       <Cards>
-        
       </Cards>
     </div>
   );

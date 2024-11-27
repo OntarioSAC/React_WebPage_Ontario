70212e911756a9da4eb5922f9dc65e1c6a4eafc2
Farid Lucio Gonzales Gonzalo <40509906+shadow3dcoo21@users.noreply.github.com>
2024-11-27 15:57:26 -0500

    responsive 101%

--- src/pages/News/Content/HeaderNews.jsx
+++ src/pages/News/Content/HeaderNews.jsx
@@ -4,16 +4,6 @@ import headerContentEN from "../../../data/news-content-en.json";
 import headerContentES from "../../../data/news-content-es.json";
 import styles from "./HeaderNews.module.css";
 
-const descriptionStyle = {
-  marginTop: "1rem",
-  fontSize: "1.2rem",
-  color: "var(--secondary-color)",
-  textAlign: "center",
-  maxWidth: "60%",
-  lineHeight: "1.4",
-  fontWeight: "400",
-  margin: "0 auto",
-};
 
 const HeaderNews = () => {
   const { i18n } = useTranslation();
@@ -51,11 +41,12 @@ const HeaderNews = () => {
         </h1>
         <p className={styles.descriptionStyle}>{headerData.description}</p>
       </div>
-      <div>
+
+      <div className={styles.lefthoja}>
              <img
                 src={headerData.lefthoja}
                
-                className={styles.lefthoja}
+                
               />
       </div>
       <div>
